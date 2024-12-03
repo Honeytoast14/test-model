14c66f6676d8c9282dd32b037f195385731aa6c3
yume_pp <num11481148@gmail.com>
2024-12-04 00:41:15 +0700

    etst

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,35 @@
 /* eslint-disable react/no-unknown-property */
-import * as THREE from 'three'
-import { Canvas} from '@react-three/fiber'
-import { Gltf, Environment, CameraControls, useVideoTexture, useGLTF } from '@react-three/drei'
-import { Suspense, useEffect } from 'react'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
 
 export default function App() {
   return (
-    <div className='App'>
-    <Canvas camera={{ position: [3, 0.5, 3] }}>
-      <Suspense fallback={null}>
-      <Room />
-      <Screen />
-
-      <Environment background>
-        <mesh scale={100}>
-          <sphereGeometry args={[1, 64, 64]} />
-          <meshBasicMaterial color="#393939" side={THREE.BackSide} />
-        </mesh>
-      </Environment>
-      <spotLight position={[7, 7, 7]} castShadow intensity={50} shadow-bias={-0.0001} />
-      <ambientLight intensity={1} />
-      <CameraControls />
-      <gridHelper args={[30, 30, 30]} position-y="0" />
-      <axesHelper args={[5]} />
-      </Suspense>
+    <Canvas orthographic>
+      <Scene />
     </Canvas>
-    </div>
   )
 }
 
-function Room() {
+function Scene() {
+  const size = useAspect(1800, 1000)
   return (
-    <>
-      <Gltf src={'/room_no_screen.glb'} />
-    </>
+    <mesh scale={size}>
+      <planeGeometry />
+      {/* <Suspense fallback={<FallbackMaterial url="10.jpg" />}> */}
+        <VideoMaterial url="/touchTheWood.mp4" />
+      {/* </Suspense> */}
+    </mesh>
   )
 }
-function Screen() {
-  const gltf = useGLTF('/screen.glb')
-  const texture = useVideoTexture('/touchTheWood.mp4')
 
-  useEffect(()=>{
-    gltf.scene.traverse((child)=>{
-      if(child.name === "screen001")
-      child.material = new THREE.MeshBasicMaterial({ map: texture });
-    })
-  },[gltf,texture])
+function VideoMaterial({ url }) {
+  const texture = useVideoTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
 
-  return (
-    <>
-      <primitive object={gltf.scene} />
-    </>
-  )
+function FallbackMaterial({ url }) {
+  const texture = useTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
 }
