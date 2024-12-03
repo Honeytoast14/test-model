e4cc9f4999f5b65bc0f8d53c1128003927e95a52
yume_pp <num11481148@gmail.com>
2024-12-04 00:19:29 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/no-unknown-property */
 import * as THREE from 'three'
 import { Canvas} from '@react-three/fiber'
-import { Gltf, Environment, CameraControls, Loader } from '@react-three/drei'
-import { Suspense } from 'react'
+import { Gltf, Environment, CameraControls, useVideoTexture, useGLTF } from '@react-three/drei'
+import { Suspense, useEffect } from 'react'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
-    <Canvas camera={{ position: [-1, 0.5, 3] }}>
+    <Canvas camera={{ position: [3, 0.5, 3] }}>
       <Suspense fallback={null}>
-      <Scene />
+      <Room />
+      <Screen />
 
       <Environment background>
         <mesh scale={100}>
@@ -21,19 +22,35 @@ export default function App() {
       <spotLight position={[7, 7, 7]} castShadow intensity={50} shadow-bias={-0.0001} />
       <ambientLight intensity={1} />
       <CameraControls />
-      <gridHelper args={[30, 30, 30]} position-y=".01" />
+      <gridHelper args={[30, 30, 30]} position-y="0" />
       <axesHelper args={[5]} />
       </Suspense>
     </Canvas>
-    <Loader/>
     </div>
   )
 }
 
-function Scene() {
+function Room() {
   return (
     <>
-      <Gltf src={'room_no_animation.glb'} />
+      <Gltf src={'/room_no_screen.glb'} />
+    </>
+  )
+}
+function Screen() {
+  const gltf = useGLTF('/screen.glb')
+  const texture = useVideoTexture('/touchTheWood.mp4')
+
+  useEffect(()=>{
+    gltf.scene.traverse((child)=>{
+      if(child.name === "screen001")
+      child.material = new THREE.MeshBasicMaterial({ map: texture });
+    })
+  },[gltf,texture])
+
+  return (
+    <>
+      <primitive object={gltf.scene} />
     </>
   )
 }
